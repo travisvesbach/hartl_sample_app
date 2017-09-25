1c8b3b0fef3212ef1d89d189cdd2584425a1bb82
Travis <tav1990@hotmail.com>
2017-09-25 14:13:40 -0500

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,10 @@ module SessionsHelper
 		cookies.permanent[:remember_token] = user.remember_token
 	end
 
+	def current_user?(user)
+		user == current_user
+	end
+
 	def current_user
 		if (user_id = session[:user_id])
 			@current_user ||= User.find_by(id: session[:user_id])
@@ -40,4 +44,15 @@ module SessionsHelper
 		@current_user = nil
 	end	
 	
+	# Redirects to stored location (or to teh default)
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	# Stores the URL trying to be accessed
+	def store_location
+		session[:forwarding_url] = request.original_url if request.get?
+	end
+
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,17 +1,33 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-  test "layout links" do
-	  get root_path
-	  assert_template "static_pages/home"
-	  assert_select "a[href=?]", root_path, count: 2
-	  assert_select "a[href=?]", help_path
-	  assert_select "a[href=?]", about_path
-	  assert_select "a[href=?]", contact_path
-	  assert_select "a[href=?]", signup_path
+
+	def setup
+		@user = users(:michael)
 	end
+
+	test "layout links while logged out" do
+	  	get root_path
+	  	assert_template "static_pages/home"
+	  	assert_select "a[href=?]", root_path, count: 2
+	  	assert_select "a[href=?]", help_path
+	  	assert_select "a[href=?]", about_path
+	  	assert_select "a[href=?]", contact_path
+	  	assert_select "a[href=?]", signup_path
+	  	assert_select "a[href=?]", users_path, count: 0
+	end
+
+	test "layout links while logged in" do
+		log_in_as(@user)
+		get root_path
+	  	assert_template "static_pages/home"
+	  	assert_select "a[href=?]", root_path, count: 2
+	  	assert_select "a[href=?]", help_path
+	  	assert_select "a[href=?]", about_path
+	  	assert_select "a[href=?]", contact_path
+	  	assert_select "a[href=?]", signup_path
+	  	assert_select "a[href=?]", users_path
+	end
+
 
 end
